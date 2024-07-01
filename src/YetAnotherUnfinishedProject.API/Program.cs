b0fe961ef7ded9df77c81d967d231c66694ec2af
oreze <22376195+oreze@users.noreply.github.com>
2024-07-01 16:14:10 +0200

    fix: add mapping controllers

--- src/YetAnotherUnfinishedProject.API/Program.cs
+++ src/YetAnotherUnfinishedProject.API/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
